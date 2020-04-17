d9f7d12f23a580cc297f4096d332e40b671cccfc
kelvinch9 <kelvinch@seas.upenn.edu>
2020-04-17 18:58:23 -0400

    consoleWriter display msgs

--- src/edu/upenn/cit594/ui/ConsoleWriter.java
+++ src/edu/upenn/cit594/ui/ConsoleWriter.java
@@ -1,7 +1,44 @@
 package edu.upenn.cit594.ui;
 
-public class ConsoleWriter {
+import java.util.Scanner;
 
+/**
+ *  Interaction class between console and user
+ *  @author Kelvin
+ *
+ */
+public class ConsoleWriter {
 	
+	static ConsoleWriter consoleWriter = null;
+	
+	public static void run() {
+		consoleWriter = new ConsoleWriter();
+		System.out.println("Welcome to OpenDataPhilly!\nPlease type your selection:");
+		displayPrompt();
+		int choice = getUserChoice();
+		while(choice < 0 || choice >6) {
+			System.out.println("Invalid selection. \n");
+			choice = getUserChoice();
+		}
+		
+	}
+	
+	public static void displayPrompt() {
+		System.out.println("0: Exit");
+		System.out.println("1: Show total population for all zip-codes");
+		System.out.println("2: Show total parking fines per capita per zip-code");
+		System.out.println("3: Show average property market value for residences in a specific zip-code");
+		System.out.println("4: Show average total livable area for residences in a specific zip-code");
+		System.out.println("5: Show total residential market value per capita in a specific zip-code");
+		System.out.println("6: Show custom feature");
+	}
+	
+	public static int getUserChoice() {
+		System.out.println("Please select a choice.");
+		Scanner scanner = new Scanner(System.in);
+		int choice = scanner.nextInt();
+		scanner.close();
+		return choice;
+	}
 	
 }
